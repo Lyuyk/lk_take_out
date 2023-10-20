977f8fbb6196158f7fcc33486eecdda12de907fb
Yingkan Lui <csap001@qq.com>
2023-10-20 15:25:07 +0800

    [Add] 完善登录功能：增加过滤器

--- src/main/java/com/lyuyk/lk/lkApplication.java
+++ src/main/java/com/lyuyk/lk/lkApplication.java
@@ -3,9 +3,11 @@ package com.lyuyk.lk;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class lkApplication {
     public static void main(String[] args) {
         SpringApplication.run(lkApplication.class);
